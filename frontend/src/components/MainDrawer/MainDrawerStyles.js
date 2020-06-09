be9b1824d0ad3163db16364712af25c16243b757
japerry911 <jack.e.perry@outlook.com>
2020-06-09 08:51:24 -0600

    Drawer draft finished

---
+++ frontend/src/components/MainDrawer/MainDrawerStyles.js
@@ -0,0 +1,10 @@
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+export const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1
+    },
+    flex: {
+        flex: 1
+    }
+}));
