9814a791cb2423d0be97c688a98cce315953d4f8
japerry911 <jack.e.perry@outlook.com>
2020-06-09 15:30:05 -0600

    Draft of users route done

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema({
         type: String,
         minlength: 6,
         maxlength: 30,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
