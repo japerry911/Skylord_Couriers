69ef4a0fea9db1d1463c47156010768dbf45d21b
japerry911 <jack.e.perry@outlook.com>
2020-06-09 09:30:01 -0600

    Footer Boilerplate setup

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useStyles } from './FooterStyles';
+
+const Footer = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <h1>Footer</h1>
+        </div>
+    );
+};
+
+export default Footer;
