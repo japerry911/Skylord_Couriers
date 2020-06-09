5088b4b81baba43161d1858f90ab5934a787b9ad
japerry911 <jack.e.perry@outlook.com>
2020-06-09 15:18:48 -0600

    Basic setup for User model created

---
+++ backend/models/user.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        minlength: 6,
+        maxlength: 30,
+        required: true
+    },
+    password: {
+        type: String,
+        minlength: 5,
+        maxlength: 1024,
+        required: true
+    },
+    isShipper: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    isCourier: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    city: {
+        type: String,
+        minlength: 2,
+        maxlength: 100,
+        required: true
+    },
+    state: {
+        type: String,
+        enum: ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS',
+                'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY',
+                'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 
+                'WI', 'WY'],
+        required: true 
+    }
+});
+
+userSchema.methods.generateAuthToken = function() {
+    return jwt.sign({ _id: this._id, isShipper: this.isShipper, isCourier: this.isCourier }, process.env.JWT_PRIVATE_KEY);
+}
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = {
+        username: Joi.string().min(6).max(30).required(),
+        password: Joi.string().required().min(5).max(255).required(),
+        isShipper: Joi.boolean().required(),
+        isCourier: Joi.boolean().required(),
+        city: Joi.string().min(2).max(100).required(),
+        state: Joi.string().valid(['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS',
+                                    'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY',
+                                    'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 
+                                    'WI', 'WY'])
+    };
+
+    return Joi.validate(user, schema);
+}
+
+module.exports.User = User;
+module.exports.validateUser = validateUser;
