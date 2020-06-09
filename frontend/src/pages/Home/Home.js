f9a4b76c4888c2d44a0859fb6c8517782525a311
japerry911 <jack.e.perry@outlook.com>
2020-06-09 13:02:29 -0600

    HomePage basic layout finished

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import { useStyles } from './HomeStyles';
 
 const Home = () => {
     const classes = useStyles();
 
     return (
-        <div>
-            <h1>Homepage</h1>
-        </div>
+        <Grid container className={classes.mainGridStyle} direction='column' alignItems='center' justify='space-evenly'>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} align='center'>
+                <img
+                    alt='Skylord Couriers Logo'
+                    src='https://skylord-couriers-12395823838.s3.us-east-2.amazonaws.com/Logos/dark_logo_transparent_background.png'
+                    className={classes.logoStyle}
+                />
+            </Grid>
+            <Grid container item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <Grid item xs={6} sm={6} md={6} lg={6} xl={6} align='center'>
+                    <Button
+                        className={classes.buttonStyle}
+                    >
+                        About Us
+                    </Button>
+                </Grid>
+                <Grid item xs={6} sm={6} md={6} lg={6} xl={6} align='center'>
+                    <Button
+                        className={classes.buttonStyle}
+                    >
+                        Sign In
+                    </Button>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 };
 
--- frontend/src/pages/Home/HomeStyles.js
+++ frontend/src/pages/Home/HomeStyles.js
@@ -1,3 +1,29 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
-export const useStyles = makeStyles(theme => ({}));
+export const useStyles = makeStyles(theme => ({
+    mainGridStyle: {
+        minHeight: '900px',
+        width: '100%',
+        backgroundImage: 'url("https://skylord-couriers-12395823838.s3.us-east-2.amazonaws.com/Home/dog-running-on-seashore-under-blue-sky-and-white-clouds-1509237.jpg")',
+        backgroundAttachment: 'fixed',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+    },
+    buttonStyle: {
+        width: '10rem',
+        height: '5rem',
+        borderColor: '#000',
+        borderWidth: '1pt',
+        borderStyle: 'solid',
+        backgroundColor: theme.colors.primaryOrange,
+        color: '#000',
+        fontSize: '1.10rem',
+        '&:hover': {
+            backgroundColor: theme.colors.accentLightOrange
+        }
+    },
+    logoStyle: {
+        width: '60%',
+        height: 'auto'
+    }
+}));
