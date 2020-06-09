469baf803bf1353fb9d09f89e93d3482a1ce64b7
japerry911 <jack.e.perry@outlook.com>
2020-06-09 15:22:58 -0600

    boilerplate setup for users routes

---
+++ backend/routes/users.js
@@ -0,0 +1,8 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const { validateUser, User } = require('../models/user');
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = router;
