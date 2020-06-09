02d8fd6ec6d2c09d932531820546a03fabe3ca1b
japerry911 <jack.e.perry@outlook.com>
2020-06-09 10:11:46 -0600

    Basic footer layout set up

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { useStyles } from './FooterStyles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 
 const Footer = () => {
     const classes = useStyles();
 
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className={classes.footerStyle}>
+            <List>
+                <ListItem>
+                    <ListItemText>Test2</ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemText>Test2</ListItemText>
+                </ListItem>
+            </List>
+            <div className={classes.spacerStyle} />
+            <figure className={classes.figureLogoStyle}>
+                <img
+                    alt='Skylord Couriers Logo'
+                    src='https://skylord-couriers-12395823838.s3.us-east-2.amazonaws.com/Logos/dark_logo_transparent_background.png'
+                    className={classes.logoStyle}
+                />
+            </figure>
         </div>
     );
 };
--- frontend/src/components/Footer/FooterStyles.js
+++ frontend/src/components/Footer/FooterStyles.js
@@ -1,3 +1,22 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
-export const useStyles = makeStyles(theme => ({}));
+export const useStyles = makeStyles(theme => ({
+    footerStyle: {
+        backgroundColor: theme.colors.primaryOrange,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    figureLogoStyle: {
+        width: '12%',
+        height: 'auto'
+    },
+    logoStyle: {
+        width: '100%',
+        height: 'auto'
+    },
+    spacerStyle: {
+        flex: 1
+    }
+}));
