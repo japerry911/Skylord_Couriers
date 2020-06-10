2201c25af177e5f32ede7285235ac6594109686e
japerry911 <jack.e.perry@outlook.com>
2020-06-10 10:25:32 -0600

    Boilerplate redux setup

---
+++ frontend/src/redux/reducers/userReducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = { user: {}, loading: false, error: null, type: null };
+
+const userReducer = (state=INITIAL_STATE, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default userReducer;
