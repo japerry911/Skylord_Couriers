11270b16dd0d7aa2558670444b75a03259eaae79
japerry911 <jack.e.perry@outlook.com>
2020-06-11 09:05:52 -0600

    Added in useEffect toolbar/navbar title setter based on location using HOC withRouter

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useStyles } from './NavBarStyles';
 import MainToolbar from '../MainToolbar/MainToolbar';
 import MainDrawer from '../MainDrawer/MainDrawer';
+import { withRouter } from 'react-router-dom';
+import { NON_AUTH_ROUTES_ARRAY, AUTH_COURIER_ROUTES_ARRAY, AUTH_SHIPPER_ROUTES_ARRAY } from '../../router/routesArrays';
 
-const NavBar = () => {
+const NavBar = ({ location }) => {
     const classes = useStyles();
 
     const [drawer, setDrawer] = useState(false);
@@ -12,6 +14,15 @@ const NavBar = () => {
     const toggleDrawer = () => {
         setDrawer(!drawer);
     };
+
+    useEffect(() => {
+        const combined = AUTH_COURIER_ROUTES_ARRAY.concat(...AUTH_SHIPPER_ROUTES_ARRAY).concat(...NON_AUTH_ROUTES_ARRAY);
+        
+        const currentPage = combined.find(routeObject => routeObject.link === location.pathname);
+        if (currentPage) {
+            setTitle(currentPage.title);
+        }
+    }, [location]);
 
     return (
         <div className={classes.root}>
@@ -28,4 +39,4 @@ const NavBar = () => {
     );
 };
 
-export default NavBar;
+export default withRouter(NavBar);
