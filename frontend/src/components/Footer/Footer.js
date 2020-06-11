661f83fbd48baeaaf6534d314bf5984cb2a19b79
japerry911 <jack.e.perry@outlook.com>
2020-06-11 08:59:51 -0600

    auth conditionals added to drawer and footer menus

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -4,12 +4,16 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import { NON_AUTH_ROUTES_ARRAY } from '../../router/routesArrays';
+import { NON_AUTH_ROUTES_ARRAY, AUTH_COURIER_ROUTES_ARRAY, AUTH_SHIPPER_ROUTES_ARRAY } from '../../router/routesArrays';
 import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
+import { useSelector } from 'react-redux';
 
 const Footer = () => {
     const classes = useStyles();
+
+    const authed = useSelector(state => state.user.authed);
+    const user = useSelector(state => state.user.user);
 
     return (
         <Grid container className={classes.footerStyle}>
@@ -27,7 +31,39 @@ const Footer = () => {
             </Grid>
             <Grid container item xs={6} sm={6} md={6} lg={6} xl={6} justify='center'>
                 <List className={classes.listStyle}>
-                    {NON_AUTH_ROUTES_ARRAY.map((routeObject, index) => {
+                    {authed && user.isShipper
+                    ?
+                    AUTH_SHIPPER_ROUTES_ARRAY.map((routeObject, index) => {
+                        return (
+                            <ListItem
+                                key={index}
+                                component={Link}
+                                to={routeObject.link}
+                                className={classes.listItemStyle}
+                            >
+                                <ListItemIcon><routeObject.Icon /></ListItemIcon>
+                                <ListItemText>{routeObject.title}</ListItemText>
+                            </ListItem>
+                        );
+                    })
+                    :
+                    authed && user.isCourier
+                    ?
+                    AUTH_COURIER_ROUTES_ARRAY.map((routeObject, index) => {
+                        return (
+                            <ListItem
+                                key={index}
+                                component={Link}
+                                to={routeObject.link}
+                                className={classes.listItemStyle}
+                            >
+                                <ListItemIcon><routeObject.Icon /></ListItemIcon>
+                                <ListItemText>{routeObject.title}</ListItemText>
+                            </ListItem>
+                        );
+                    })
+                    :
+                    NON_AUTH_ROUTES_ARRAY.map((routeObject, index) => {
                         return (
                             <ListItem
                                 key={index}
