2c07d7e15245b65f73f0d305e974dfea63bd0e3c
japerry911 <jack.e.perry@outlook.com>
2020-06-11 17:12:49 -0600

    ShipmentGood model draft finished

---
+++ backend/models/shipmentGood.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const shipmentGoodSchema = new mongoose.Schema({
+    good: {
+        type: new mongoose.Schema({
+            name: {
+                type: String,
+                required: true,
+                minlength: 2,
+                maxlength: 100
+            },
+            weight: {
+                type: Number,
+                required: true,
+                min: 0.01,
+                max: 1000
+            }
+        }),
+        required: true
+    },
+    shipmentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
+});
+
+const ShipmentGood = mongoose.model('ShipmentGood', shipmentGoodSchema);
+
+function validateShipmentGood(shipmentGood) {
+    const schema = {
+        goodId: Joi.objectId().required(),
+        shipmentId: Joi.objectId().required()
+    };
+
+    return Joi.validate(shipmentGood, schema);
+}
+
+module.exports.ShipmentGood = ShipmentGood;
+module.exports.validateShipmentGood = validateShipmentGood;
