1af5c34ee7f0e326b93b24442f07ddf0480c3393
japerry911 <jack.e.perry@outlook.com>
2020-06-13 15:48:25 -0600

    basic showShipment page draft finished

--- frontend/src/pages/ShowShipment/ShowShipment.js
+++ frontend/src/pages/ShowShipment/ShowShipment.js
@@ -2,6 +2,8 @@ import React, { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getShipment } from '../../redux/actions/shipmentActions';
 import LoadingOverlay from 'react-loading-overlay';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import { useStyles } from './ShowShipmentStyles';
 
 const ShowShipment = ({ match }) => {
@@ -15,7 +17,7 @@ const ShowShipment = ({ match }) => {
 
     useEffect(() => {
         dispatch(getShipment(token, match.params.id));
-    }, [dispatch, token]);
+    }, [dispatch, token, match.params.id]);
 
     return (
         <Fragment>
@@ -24,9 +26,81 @@ const ShowShipment = ({ match }) => {
                 spinner
                 text='Loading...'
             >
-                <div>   
-                    <h1>Shipment - {match.params.id}</h1>
-                </div>
+                <Grid container className={classes.mainGridStyle} direction='column' alignItems='center' justify='space-evenly'>
+                    <Grid item xs={8} sm={8} md={8} lg={8} xl={8} align='center' className={classes.gridItemStyle}>
+                        <img
+                            alt='Skylord Couriers Logo'
+                            src='https://skylord-couriers-12395823838.s3.us-east-2.amazonaws.com/Logos/white_logo_transparent_background.png'
+                            className={classes.logoStyle}
+                        />
+                    </Grid>
+                    <Grid container item xs={8} sm={8} md={8} lg={8} xl={8} alignItems='center' direction='column' className={classes.subGridContainerStyle}>
+                        <Grid item xs={10} sm={10} md={10} lg={10} xl={10} align='center' className={classes.gridItemStyle}>
+                            <Typography
+                                variant='h6'
+                                className={classes.headerTextStyle}
+                            >
+                                Shipment #{match.params.id}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={10} sm={10} md={10} lg={10} xl={10} align='center' className={classes.gridItemStyle}>
+                            <Typography
+                                variant='body1'
+                                className={classes.textStyle}
+                            >
+                                Status - {showShipment.status}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={10} sm={10} md={10} lg={10} xl={10} align='center' className={classes.gridItemStyle}>
+                            <Typography
+                                variant='body1'
+                                className={classes.textStyle}
+                            >
+                                Posted Date - {showShipment.postDate ? showShipment.postDate.substring(0, 10) : 'n/a'}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={10} sm={10} md={10} lg={10} xl={10} align='center' className={classes.gridItemStyle}>
+                            <Typography
+                                variant='body1'
+                                className={classes.textStyle}
+                            >
+                                Shipper - {showShipment.shipper ? showShipment.shipper.username : 'n/a'}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={10} sm={10} md={10} lg={10} xl={10} align='center' className={classes.gridItemStyle}>
+                            <Typography
+                                variant='body1'
+                                className={classes.textStyle}
+                            >
+                                Courier - {showShipment.courier ? showShipment.courier.username : 'n/a'}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={10} sm={10} md={10} lg={10} xl={10} align='center' className={classes.gridItemStyle}>
+                            <Typography
+                                variant='body1'
+                                className={classes.textStyle}
+                            >
+                                Start Date - {showShipment.startDate ? showShipment.startDate.substring(0, 10) : 'n/a'}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={10} sm={10} md={10} lg={10} xl={10} align='center' className={classes.gridItemStyle}>
+                            <Typography
+                                variant='body1'
+                                className={classes.textStyle}
+                            >
+                                Delivered Date - {showShipment.deliveredDate ? showShipment.deliveredDate.substring(0, 10) : 'n/a'}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={10} sm={10} md={10} lg={10} xl={10} align='center' className={classes.gridItemStyle}>
+                            <Typography
+                                variant='body1'
+                                className={classes.textStyle}
+                            >
+                                Comment(s) - {showShipment.comments ? showShipment.comments : 'n/a'}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </LoadingOverlay>
         </Fragment>
     );
--- frontend/src/pages/ShowShipment/ShowShipmentStyles.js
+++ frontend/src/pages/ShowShipment/ShowShipmentStyles.js
@@ -1,3 +1,35 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
-export const useStyles = makeStyles(theme => ({}));
+export const useStyles = makeStyles(theme => ({
+    mainGridStyle: {
+        minHeight: '900px',
+        width: '100%',
+        backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),' +
+            'url("https://skylord-couriers-12395823838.s3.us-east-2.amazonaws.com/ShowShipment/leone-venter-mTkXSSScrzw-unsplash.jpg")',
+        backgroundAttachment: 'fixed',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'bottom'
+    },
+    subGridContainerStyle: {
+        backgroundColor: theme.colors.accentLightOrange,
+        borderRadius: '12pt',
+        padding: '2rem',
+        minHeight: '600px',
+        marginBottom: '3rem'
+    },
+    gridItemStyle: {
+        padding: '2rem 0'
+    },
+    headerTextStyle: {
+        color: '#000',
+        fontWeight: 'bold'
+    },
+    textStyle: {
+        color: '#000'
+    },
+    logoStyle: {
+        height: 'auto',
+        width: '25%'
+    }
+}));
