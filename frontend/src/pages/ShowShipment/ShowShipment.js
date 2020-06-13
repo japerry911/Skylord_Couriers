7b7062ba0f23ecc0b0cc4ae7b7e2d0f2368c3efa
japerry911 <jack.e.perry@outlook.com>
2020-06-13 09:46:17 -0600

    integrated getShipment into ShowShipment component, and added basic overlay loading spinner

--- frontend/src/pages/ShowShipment/ShowShipment.js
+++ frontend/src/pages/ShowShipment/ShowShipment.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getShipment } from '../../redux/actions/shipmentActions';
+import LoadingOverlay from 'react-loading-overlay';
 import { useStyles } from './ShowShipmentStyles';
 
 const ShowShipment = ({ match }) => {
     const classes = useStyles();
 
+    const dispatch = useDispatch();
+    const isLoading = useSelector(state => state.shipments.isLoading);
+    const showShipment = useSelector(state => state.shipments.showShipment);
+    const error = useSelector(state => state.shipments.error);
+    const token = useSelector(state => state.user.token);
+
+    useEffect(() => {
+        dispatch(getShipment(token, match.params.id));
+    }, [dispatch, token]);
+
     return (
-        <div>
-            <h1>Shipment - {match.params.id}</h1>
-        </div>
+        <Fragment>
+            <LoadingOverlay
+                active={isLoading}
+                spinner
+                text='Loading...'
+            >
+                <div>   
+                    <h1>Shipment - {match.params.id}</h1>
+                </div>
+            </LoadingOverlay>
+        </Fragment>
     );
 };
 
